01178ee6c9aa594b4f25d3f9a4c9db5f3b0b3c0d
SANJAI-SANSPARK <sanjaisenthil2k@gmail.com>
2024-11-21 13:28:31 +0530

    adding right view of the binary tree program

---
+++ 21_11_24_DSA/BinaryTreeRightSideView.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+ 
+
+
+ class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> q = new ArrayList<>();
+
+        if(root == null)
+           return res;
+
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int l = q.size();
+
+            for(int i=0;i<l;i++){
+                TreeNode tem = q.poll();
+                if(i==l-1){
+                    res.add(tem.val);
+                }
+                
+                if(tem.left != null)
+                   q.add(tem.left);
+                if(tem.right != null)
+                  q.add(tem.right);
+            }
+        }
+
+        return res;
+    }
+}
