59ea3218774d45f7956ce3bc3258fad495df71d1
SANJAI-SANSPARK <sanjaisenthil2k@gmail.com>
2024-11-22 13:00:56 +0530

    added Paths program

---
+++ 22_11_24_DSA/RoottoLeafPaths.java
@@ -0,0 +1,48 @@
+Definition for Binary Tree Node
+class Node
+{
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+*/
+
+class Solution {
+    public static ArrayList<ArrayList<Integer>> Paths(Node root) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if(root == null)
+           return res;
+           
+        ArrayList<Integer> tem = new ArrayList<>();
+        rec(root , tem , res);
+        return res;
+    }
+    
+    public static void rec(Node root ,ArrayList<Integer> tem,ArrayList<ArrayList<Integer>> res){
+        
+        tem.add(root.data);
+        if(root.left == null && root.right == null){
+            res.add(new ArrayList<>(tem));
+            return;
+        }
+        
+        if(root.left != null){
+            rec(root.left , tem , res);
+            tem.remove(tem.size()-1);
+        }
+        
+        if(root.right != null){
+            rec(root.right , tem , res);
+            tem.remove(tem.size()-1);
+        }
+        
+    }
+}
+
